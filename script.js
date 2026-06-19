24e368b2068495bb7abd5d99439bb8be4daf37bf
Kevinmatthew1011 <kevinmatthew1011@gmail.com>
2026-06-19 22:48:33 +0530

    Add local storage support to todo app

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+function saveTasks() {
+    const tasks = [];
+
+    document.querySelectorAll("#taskList span").forEach(task => {
+        tasks.push({
+            text: task.textContent,
+            completed: task.classList.contains("completed")
+        });
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 function addTask() {
     const input = document.getElementById("taskInput");
     const taskList = document.getElementById("taskList");
@@ -13,6 +25,7 @@ function addTask() {
 
     taskText.addEventListener("click", () => {
         taskText.classList.toggle("completed");
+        saveTasks();
     });
 
     const deleteBtn = document.createElement("button");
@@ -21,13 +34,15 @@ function addTask() {
     deleteBtn.addEventListener("click", (event) => {
         event.stopPropagation();
         li.remove();
+        saveTasks();
+
     });
 
     li.appendChild(taskText);
     li.appendChild(deleteBtn);
 
     taskList.appendChild(li);
-
+    saveTasks();
     input.value = "";
 }
 
@@ -38,3 +53,38 @@ document
             addTask();
         }
     });
+    function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    tasks.forEach(task => {
+        const li = document.createElement("li");
+
+        const taskText = document.createElement("span");
+        taskText.textContent = task.text;
+
+        if (task.completed) {
+            taskText.classList.add("completed");
+        }
+
+        taskText.addEventListener("click", () => {
+            taskText.classList.toggle("completed");
+            saveTasks();
+        });
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+
+        deleteBtn.addEventListener("click", (event) => {
+            event.stopPropagation();
+            li.remove();
+            saveTasks();
+        });
+
+        li.appendChild(taskText);
+        li.appendChild(deleteBtn);
+
+        document.getElementById("taskList").appendChild(li);
+    });
+}
+
+loadTasks();
